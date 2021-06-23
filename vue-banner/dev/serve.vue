03967b2acbc06f3ccf991461caad360914f199c5
Snargol <anatole.couasnon@orange.fr>
2021-06-23 11:53:16 +0200

    ended

---
+++ vue-banner/dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueBanner from '@/vue-banner.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueBanner
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <VueBanner/>
+  </div>
+</template>
---
+++ vue-banner/src/vue-banner.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  this is a text
+</div>
+</template>
+
+<script>
+
+export default {
+  name: "VueBanner"
+}
+</script>
+
+<style scoped>
+
+</style>
